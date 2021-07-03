c6154cdbaf0cd7c7f86434b18943ae5244ba1e7d
Kiran <kiran@cedextech.com>
2021-07-03 15:28:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const https = require('https');
+
+class Unsplash {
+
+    constructor(config) {
+        this.client_id = config.access_key;
+        this.basePath = 'https://api.unsplash.com';
+    }
+
+    request(url, options) {
+        let url = new URL(this.basePath + path);
+        let result = '';
+
+        let config = {
+            headers: {
+                Authorization: `Client-ID ${this.client_id}`
+            }
+        }
+
+        if('query' in options) {
+            url.searchParams.set('query', options.query);
+            url.searchParams.set('per_page', 10);
+        }
+
+        // console.log(url);
+
+        return new Promise((resolve, reject) => {
+            https.get(url, config, (response) => {
+                response.setEncoding('utf8');
+
+                response.on('data', function(data) {
+                    result += data;
+                });
+
+                response.on('end', function() {
+                    resolve(JSON.parse(result));
+                })
+
+                response.on('error', function () {
+                    reject(error);
+                });
+            });
+        });
+    }
+
+    async searchPhotos(options) {
+        let path = '/search/photos';
+
+        return await this.request(path, options);
+    }
+
+    async getPhoto(options) {
+        let path = '/photos/' + options.photo_id;
+
+        return await this.request(path, options);
+    }
+}
+
+module.exports = Unsplash;
